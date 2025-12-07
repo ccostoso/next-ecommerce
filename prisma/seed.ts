88d196ffda3b865ebb2ac5f36c0ef69bdc4f1bbc
Carlos Costoso <carlos.costoso@outlook.com>
2025-12-07 15:16:38 -0500

    Seeds categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,38 @@
-console.log("Seeding database...");
+import { prisma } from "../lib/prisma";
+
+async function main() {
+	await prisma.product.deleteMany();
+	await prisma.category.deleteMany();
+
+	const electronics = await prisma.category.create({
+		data: {
+			name: "Electronics",
+			slug: "electronics",
+		},
+	});
+
+	const clothing = await prisma.category.create({
+		data: {
+			name: "Clothing",
+			slug: "clothing",
+		},
+	});
+
+	const home = await prisma.category.create({
+		data: {
+			name: "Home",
+			slug: "home",
+		},
+	});
+}
+
+main()
+	.then(async () => {
+		console.log("Seeding complete!");
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
