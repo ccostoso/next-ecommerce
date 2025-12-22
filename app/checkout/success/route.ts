6589093630c4e9761decaa2be94a0216450c6fa4
Carlos Costoso <carlos.costoso@outlook.com>
2025-12-22 14:55:50 -0500

    Adds success route handler

--- app/checkout/success/route.ts
+++ app/checkout/success/route.ts
@@ -1,7 +1,38 @@
+import prisma from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
+import { notFound, redirect } from "next/navigation";
 import { type NextRequest } from "next/server";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
 	const searchParams = request.nextUrl.searchParams;
-	const query = searchParams.get("session_id");
-	// query is "hello" for /api/search?query=hello
+	const sessionId = searchParams.get("session_id");
+
+	if (!sessionId) notFound();
+
+	try {
+		const session = await stripe.checkout.sessions.retrieve(sessionId);
+		const orderId = session.metadata?.orderId;
+
+		if (!orderId) notFound();
+
+		const order = await prisma.order.findUnique({
+			where: { id: orderId, stripeSessionId: sessionId },
+		});
+
+		if (!order) notFound();
+
+		if (order.status === "PENDING_PAYMENT") {
+			await prisma.order.update({
+				where: { id: order.id },
+				data: { status: "PAYMENT_PROCESSED" },
+			});
+		}
+
+		return redirect("");
+	} catch (error) {
+		console.error("Error retrieving session or order:", error);
+		notFound();
+	}
+
+	return redirect("/");
 }
