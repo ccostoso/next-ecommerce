6fa51af6827a1b2a8a33bfc3ca23db895ff52d5f
Carlos Costoso <carlos.costoso@outlook.com>
2025-12-23 21:15:13 -0500

    Adds shadcn Field to signin page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,20 +4,30 @@ import {
 	Card,
 	CardContent,
 	CardDescription,
+	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { SigninSchemaType } from "@/lib/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SigninSchema } from "@/lib/schemas";
+import {
+	Field,
+	FieldDescription,
+	FieldGroup,
+	FieldLabel,
+} from "@/components/ui/field";
 
 export default function SignInPage() {
-	const form = useForm<SigninSchemaType>({
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<SigninSchemaType>({
 		resolver: zodResolver(SigninSchema),
 		defaultValues: {
 			email: "",
@@ -31,45 +41,74 @@ export default function SignInPage() {
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center container mx-auto p-4">
-			<Card className="w-full max-w-md p-6">
+			<Card className="w-full max-w-md">
 				<CardHeader>
-					<CardTitle>Sign In</CardTitle>
+					<CardTitle className="text-2xl">Welcome Back</CardTitle>
 					<CardDescription>
-						Or{" "}
+						Sign in to your account to continue.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form
+						className="w-full max-w-md"
+						onSubmit={handleSubmit(onSubmit)}
+					>
+						<FieldGroup className="gap-5">
+							<Field>
+								<FieldLabel htmlFor="email">Email</FieldLabel>
+								<Input
+									type="email"
+									id="email"
+									{...register("email")}
+									aria-invalid={!!errors.email}
+									placeholder="Email"
+								/>
+								{errors.email && (
+									<FieldDescription className="text-destructive text-sm">
+										{errors.email.message}
+									</FieldDescription>
+								)}
+							</Field>
+
+							<Field>
+								<FieldLabel htmlFor="password">
+									Password
+								</FieldLabel>
+								<Input
+									type="password"
+									id="password"
+									{...register("password")}
+									aria-invalid={!!errors.password}
+									placeholder="Password"
+								/>
+								{errors.password && (
+									<FieldDescription className="text-destructive text-sm">
+										{errors.password.message}
+									</FieldDescription>
+								)}
+							</Field>
+
+							<Button
+								type="submit"
+								className="w-full mt-2 bg-green-600 hover:bg-green-700 hover:shadow-lg transition-shadow text-white"
+							>
+								Sign In
+							</Button>
+						</FieldGroup>
+					</form>
+				</CardContent>
+				<CardFooter>
+					<p className="text-sm text-muted-foreground">
+						Don't have an account?{" "}
 						<Link
 							href="/auth/signup"
 							className="font-medium text-primary hover:underline"
 						>
-							create an account
+							Register here
 						</Link>
 						.
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<form className="space-y-4">
-						<div className="space-y-2">
-							<Label
-								htmlFor="email"
-								className="text-sm font-medium"
-							>
-								Email
-							</Label>
-							<Input type="email" id="email" required />
-						</div>
-						<div className="space-y-2">
-							<Label
-								htmlFor="password"
-								className="text-sm font-medium"
-							>
-								Password
-							</Label>
-							<Input type="password" id="password" required />
-						</div>
-						<Button type="submit" className="w-full mt-4">
-							Sign In
-						</Button>
-					</form>
-				</CardContent>
+					</p>
+				</CardFooter>
 			</Card>
 		</main>
 	);
