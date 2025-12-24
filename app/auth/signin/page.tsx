3e7467e02c7f7bdf146f4a9c65a3e53620bfbdcb
Carlos Costoso <carlos.costoso@outlook.com>
2025-12-23 19:37:24 -0500

    Adds signin ui shell and configures authjs signin page

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,58 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export default function SignInPage() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center container mx-auto p-4">
+			<Card className="w-full max-w-md p-6">
+				<CardHeader>
+					<CardTitle>Sign In</CardTitle>
+					<CardDescription>
+						Or{" "}
+						<Link
+							href="/auth/signup"
+							className="font-medium text-primary hover:underline"
+						>
+							create an account
+						</Link>
+						.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form className="space-y-4">
+						<div className="space-y-2">
+							<Label
+								htmlFor="email"
+								className="text-sm font-medium"
+							>
+								Email
+							</Label>
+							<Input type="email" id="email" required />
+						</div>
+						<div className="space-y-2">
+							<Label
+								htmlFor="password"
+								className="text-sm font-medium"
+							>
+								Password
+							</Label>
+							<Input type="password" id="password" required />
+						</div>
+						<Button type="submit" className="w-full mt-4">
+							Sign In
+						</Button>
+					</form>
+				</CardContent>
+			</Card>
+		</main>
+	);
+}
