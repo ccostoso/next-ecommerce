533edd382c1223d37dd8d8e1404efab7ced8d0c6
Carlos Costoso <carlos.costoso@outlook.com>
2026-01-05 15:55:08 -0500

    Adds credentials provider logic 2

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,57 @@
 import NextAuth from "next-auth";
 import bcrypt from "bcryptjs";
+import Credentials from "next-auth/providers/credentials";
+import { SigninSchema } from "./schemas";
+import prisma from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [],
+	providers: [
+		Credentials({
+			name: "Credentials",
+			credentials: {
+				email: {},
+				password: {},
+			},
+			async authorize(credentials) {
+				const parsedCredentials = SigninSchema.safeParse(credentials);
+				if (!parsedCredentials.success) {
+					console.log("Invalid credentials format");
+					return null;
+				}
+
+				const { email, password } = parsedCredentials.data;
+
+				try {
+					const user = await prisma.user.findUnique({
+						where: { email },
+					});
+
+					if (!user) {
+						console.log("User not found");
+						return null;
+					}
+
+					const isPasswordValid = await comparePassword(
+						password,
+						user.password
+					);
+
+					if (!isPasswordValid) {
+						console.log("Invalid password");
+						return null;
+					}
+
+					return {
+						id: user.id,
+						email: user.email,
+					};
+				} catch (error) {
+					console.error("Error during authentication:", error);
+					return null;
+				}
+			},
+		}),
+	],
 	pages: {
 		signIn: "/auth/signin",
 	},
